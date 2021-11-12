29d6d185b537d01dbd52977b31c9ae20ddd0a538
kevinthon <kevin.thon@gmail.com>
2021-11-12 13:40:08 +0100

    indikatorfane

--- R/nraBeregnIndikator.R
+++ R/nraBeregnIndikator.R
@@ -12,10 +12,11 @@ nraBeregnIndikator <- function(RegData, valgtVar) {
   kobl_resh_orgnr <- data.frame(resh = c(601225, 108162, 107440, 700116, 700922, 111138, 107505, 4210588),
                                 orgnr = c(974795787, 974706490, 974749025, 983971768, 974557746, 974724960, 974116804, 974733013),
                                 shus = c("UNN", "Akershus", "St.Olav", "Østfold", "Haukeland", "Innlandet", "DS", "Kristiansand"))
-  nraUtvalg <- nraUtvalg(RegData=RegData, forlopstype1=2)
-  RegData <- nraUtvalg$RegData
+
 
   if (valgtVar == "Indikator1_lekk_red50") {
+    nraUtvalg <- nraUtvalg(RegData=RegData, forlopstype1=2)
+    RegData <- nraUtvalg$RegData
     indikator <- RegData[!is.na(RegData$Indikator1_lekk_red50), c("Aar", "AvdRESH", "Indikator1_lekk_red50")]
     names(indikator)[names(indikator)=="Indikator1_lekk_red50"] <- "var"
     names(indikator)[names(indikator)=="Aar"] <- "year"
@@ -23,6 +24,21 @@ nraBeregnIndikator <- function(RegData, valgtVar) {
     indikator$ind_id <- 'nra_50pst_lekkasjeredusjon'
     indikator$orgnr <- kobl_resh_orgnr$orgnr[match(indikator$AvdRESH, kobl_resh_orgnr$resh)]
     indikator$SenterKortNavn <- kobl_resh_orgnr$shus[match(indikator$AvdRESH, kobl_resh_orgnr$resh)]
+  }
+
+  if (valgtVar == "Ultralyd") {
+    indikator <- RegData[which(RegData$Ultralyd %in% 0:2), ]
+    indikator <- indikator[indikator$ForlopsType1Num %in% c(1,2) & indikator$ForlopsType2Num %in% c(1,2,5, NA), ]
+
+    indikator <- indikator[ , c("Aar", "AvdRESH", "PasientID", "ForlopsID", "Sfinktervurdering")]
+    indikator$var <- indikator$Sfinktervurdering
+    indikator$var <- as.numeric(indikator$var != 99)
+    indikator$denominator <- 1
+    indikator$ind_id <- 'nra_ultralyd'
+    names(indikator)[names(indikator)=="Aar"] <- "year"
+    indikator$orgnr <- kobl_resh_orgnr$orgnr[match(indikator$AvdRESH, kobl_resh_orgnr$resh)]
+    indikator$SenterKortNavn <- kobl_resh_orgnr$shus[match(indikator$AvdRESH, kobl_resh_orgnr$resh)]
+    indikator <- indikator[, c("orgnr",	"year",	"var",	"denominator",	"ind_id", "AvdRESH", "SenterKortNavn")]
   }
 
 
@@ -35,8 +51,25 @@ return(indikator)
 
 
 
-
-
+#
+# names(Indikatorer) <- c("orgnr",	"year",	"var",	"denominator",	"ind_id")
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind1"] <- "nra_50pst_lekkasjeredusjon"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind2"] <- "nra_ultralyd"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind3"] <- "nra_saarinfeksjon"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind4"] <- "nra_stmarks_9_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind5"] <- "nra_stmarks_9_5aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind6"] <- "nra_stmarks_12_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind7"] <- "nra_stmarks_12_5aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind8"] <- "nra_stmarks_9_1aar_sfinkt"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind9"] <- "nra_stmarks_9_5aar_sfinkt"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind10"] <- "nra_stmarks_12_1aar_sfinkt"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind11"] <- "nra_stmarks_12_5aar_sfinkt"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind12"] <- "nra_wexner_9_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind13"] <- "nra_wexner_12_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind14"] <- "nra_inkontinensscore_9_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind15"] <- "nra_inkontinensscore_9_1aar_sfinkt"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind16"] <- "nra_inkontinensscore_12_1aar_snm"
+# Indikatorer$ind_id[Indikatorer$ind_id == "Ind17"] <- "nra_inkontinensscore_12_1aar_sfinkt"
 
 
 
---
+++ inst/shinyApps/nra/R/modul_indikatorfig.R
@@ -0,0 +1,212 @@
+# Modul for indikatorfigurer i NRA sin shiny-app på Rapporteket
+#
+# Kun til bruk i Shiny
+#
+#
+indikatorfig_UI <- function(id){
+  ns <- shiny::NS(id)
+
+  shiny::sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = ns("valgtVar"), label = "Velg variabel",
+                  choices = c("Indikator1_lekk_red50", "Ultralyd")),
+      uiOutput(outputId = ns('tilAar'))
+      # dateRangeInput(inputId=ns("datovalg"), label = "Dato fra og til",
+      #                max = Sys.Date(), start  = '2014-01-01', end = Sys.Date(), language = "nb", separator = " til "),
+      # selectInput(inputId = ns("enhetsUtvalg"), label = "Lag figur for",
+      #             choices = c('Hele landet'=0, 'Egen avd. mot landet forøvrig'=1, 'Egen avd.'=2)),
+      # selectInput(inputId = ns("valgtShus"), label = "Velg sykehus",
+      #             choices = BrValg$sykehus, multiple = TRUE),
+      # sliderInput(inputId=ns("alder"), label = "Alder", min = 0,
+      #             max = 130, value = c(0, 130)),
+      # selectInput(inputId = ns("erMann"), label = "Kjønn",
+      #             choices = c('Begge'=99, 'Kvinne'=0, 'Mann'=1)),
+      # selectInput(inputId = ns("forlopstype1"), label = "Velg operasjonstype",
+      #             choices = c('--'=99, 'Sfinkterplastikk'=1, 'SNM'=2)),
+      # uiOutput(outputId = ns('forlopstype2')),
+      # selectInput(inputId = ns("bildeformat"), label = "Velg bildeformat",
+      #             choices = c('pdf', 'png', 'jpg', 'bmp', 'tif', 'svg'))
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(id = ns("tab"),
+                  tabPanel("Figur", value = "fig",
+                           plotOutput(ns("Figur1"), height="auto"), downloadButton(ns("lastNedBilde"), "Last ned figur")),
+                  tabPanel("Tabell", value = "tab",
+                           # uiOutput(ns("utvalg")),
+                           tableOutput(ns("Tabell1")), downloadButton(ns("lastNed"), "Last ned tabell")
+                  )
+      )
+    )
+  )
+}
+
+
+indikatorfig <- function(input, output, session, RegData, hvd_session){
+
+  output$tilAar <- renderUI({
+    ns <- session$ns
+    selectInput(inputId = ns("tilAar_verdi"), label = "T.o.m. år",
+                choices = rev((min(RegData$Aar, na.rm = T)+2):max(RegData$Aar, na.rm = T)))
+  })
+
+
+
+  indikatorData <- reactive({
+    TabellData <- nraBeregnIndikator(RegData=RegData, valgtVar=input$valgtVar)
+    TabellData <- TabellData[which(TabellData$year <= as.numeric(req(input$tilAar_verdi))), ]
+  })
+
+  output$Figur1 <- renderPlot({
+    indikator <- req(indikatorData())
+    plotdata <- indikator[, c('AvdRESH', 'year', 'var', "SenterKortNavn")]
+    names(plotdata) <- c('ReshId', 'Aar', 'Teller', "SenterKortNavn")
+    nraFigIndikator(plotdata, tittel = c('Andel med prosentvis reduksjon', 'i lekkasjeepisoder >= 50%')
+                    , terskel = 5, maal = 70, outfile='')
+  }, width = 700, height = 700)
+
+  output$Tabell1 <- renderTable(
+
+    Tabell <- req(indikatorData()) %>% dplyr::group_by(SenterKortNavn, year) %>%
+      dplyr::summarise(Antall = sum(var),
+                       N = n(),
+                       Andel = Antall/N*100)
+  )
+
+
+
+  # plotdata <- indikator[, c('AvdRESH', 'year', 'var')]
+  # names(plotdata) <- c('ReshId', 'Aar', 'Teller')
+  # plotdata$SenterKortNavn <- RegData$SenterKortNavn[match(plotdata$ReshId, RegData$AvdRESH)]
+
+  # output$Figur1 <- renderPlot({
+  #   nraFigAndeler(RegData = RegData, valgtVar = input$valgtVar, minald=as.numeric(input$alder[1]),
+  #                 maxald=as.numeric(input$alder[2]), datoFra = input$datovalg[1], datoTil = input$datovalg[2],
+  #                 valgtShus = if (!is.null(input$valgtShus)) {input$valgtShus} else {''}, outfile = '', preprosess=F,
+  #                 erMann = as.numeric(input$erMann), reshID = reshID, enhetsUtvalg = input$enhetsUtvalg, hentData=F,
+  #                 forlopstype1=as.numeric(input$forlopstype1), forlopstype2=if(!is.null(input$forlopstype2_verdi)){as.numeric(input$forlopstype2_verdi)}
+  #                 else {99})
+  # }, width = 700, height = 700)
+
+
+  # tabellReager <- reactive({
+  #   TabellData <- nraFigAndeler(RegData = RegData, valgtVar = input$valgtVar, minald=as.numeric(input$alder[1]),
+  #                               maxald=as.numeric(input$alder[2]), datoFra = input$datovalg[1], datoTil = input$datovalg[2],
+  #                               valgtShus = if (!is.null(input$valgtShus)) {input$valgtShus} else {''}, outfile = '', preprosess=F,
+  #                               erMann = as.numeric(input$erMann), reshID = reshID, enhetsUtvalg = input$enhetsUtvalg, hentData=F,
+  #                               forlopstype1=as.numeric(input$forlopstype1), forlopstype2=if(!is.null(input$forlopstype2_verdi)){as.numeric(input$forlopstype2_verdi)}
+  #                               else {99})
+  # })
+  #
+  # output$utvalg <- renderUI({
+  #   TabellData <- tabellReager()
+  #   tagList(
+  #     h3(HTML(paste0(TabellData$tittel, '<br />'))),
+  #     h5(HTML(paste0(TabellData$utvalgTxt, '<br />')))
+  #   )})
+
+
+
+  # output$Tabell1 <- function() {
+  #
+  #   TabellData <- tabellReager()
+  #   if (input$enhetsUtvalg == 1) {
+  #     Tabell1 <- as_tibble(TabellData$TabellData) %>%
+  #       mutate('Kategori'=TabellData$grtxt, 'AndelHoved'=AntHoved/NHoved*100, 'AndelRest'=AntRest/NRest*100) %>%
+  #       select(Kategori, 1:2, AndelHoved, 3:4, AndelRest) %>%
+  #       rename(Antall=AntHoved, N=NHoved, Andel=AndelHoved, Antall=AntRest, N=NRest, Andel=AndelRest) %>%
+  #       knitr::kable("html", digits = c(0,0,0,1,0,0,1)) %>%
+  #       kable_styling("hover", full_width = F) %>%
+  #       add_header_above(c(" ", "Din avdeling" = 3, "Landet forøvrig" = 3))
+  #   } else {
+  #     Tabell1 <- as_tibble(TabellData$TabellData) %>%
+  #       mutate('Kategori'=TabellData$grtxt, 'Andel'=AntHoved/NHoved*100) %>%
+  #       select(Kategori, 1:2, Andel) %>%
+  #       rename(Antall=AntHoved, N=NHoved) %>%
+  #       knitr::kable("html", digits = c(0,0,0,1)) %>%
+  #       kable_styling("hover", full_width = F)
+  #   }
+  # }
+
+  # output$lastNed <- downloadHandler(
+  #   filename = function(){
+  #     paste0(input$valgtVar, Sys.time(), '.csv')
+  #   },
+  #
+  #   content = function(file){
+  #     TabellData <- tabellReager()
+  #     if (input$enhetsUtvalg == 1) {
+  #       Tabell1 <- as_tibble(TabellData$TabellData) %>%
+  #         mutate('Kategori'=TabellData$grtxt, 'AndelHoved'=AntHoved/NHoved*100, 'AndelRest'=AntRest/NRest*100) %>%
+  #         select(Kategori, 1:2, AndelHoved, 3:4, AndelRest)
+  #     } else {
+  #       Tabell1 <- as_tibble(TabellData$TabellData) %>%
+  #         mutate('Kategori'=TabellData$grtxt, 'Andel'=AntHoved/NHoved*100) %>%
+  #         select(Kategori, 1:2, Andel)
+  #     }
+  #     write.csv2(Tabell1, file, row.names = F)
+  #   }
+  # )
+  #
+  # output$lastNedBilde <- downloadHandler(
+  #   filename = function(){
+  #     paste0(input$valgtVar, Sys.time(), '.', input$bildeformat)
+  #   },
+  #
+  #   content = function(file){
+  #     nra::nraFigAndeler(RegData = RegData, valgtVar = input$valgtVar, minald=as.numeric(input$alder[1]),
+  #                        maxald=as.numeric(input$alder[2]), datoFra = input$datovalg[1], datoTil = input$datovalg[2],
+  #                        valgtShus = if (!is.null(input$valgtShus)) {input$valgtShus} else {''}, preprosess=F,
+  #                        erMann = as.numeric(input$erMann), reshID = reshID, enhetsUtvalg = input$enhetsUtvalg, hentData=F,
+  #                        forlopstype1=as.numeric(input$forlopstype1), forlopstype2=if(!is.null(input$forlopstype2_verdi)){as.numeric(input$forlopstype2_verdi)}
+  #                        else {99}, outfile = file)
+  #   }
+  # )
+
+
+  # shiny::observe({
+  #   if (rapbase::isRapContext()) {
+  #     if (req(input$tab) == "fig") {
+  #       mld_fordeling <- paste0(
+  #         "NRA: Figur - fordeling, variabel - ",
+  #         input$valgtVar)
+  #     }
+  #     if (req(input$tab) == "tab") {
+  #       mld_fordeling <- paste(
+  #         "NRA: tabell - fordeling. variabel - ",
+  #         input$valgtVar)
+  #     }
+  #     rapbase::repLogger(
+  #       session = hvd_session,
+  #       msg = mld_fordeling
+  #     )
+  #     mldLastNedFig <- paste(
+  #       "NRA: nedlasting figur - fordeling. variabel -",
+  #       input$valgtVar
+  #     )
+  #     mldLastNedTab <- paste(
+  #       "NRA: nedlasting tabell - fordeling. variabel -",
+  #       input$valgtVar
+  #     )
+  #     shinyjs::onclick(
+  #       "lastNedBilde",
+  #       rapbase::repLogger(
+  #         session = hvd_session,
+  #         msg = mldLastNedFig
+  #       )
+  #     )
+  #     shinyjs::onclick(
+  #       "lastNed",
+  #       rapbase::repLogger(
+  #         session = hvd_session,
+  #         msg = mldLastNedTab
+  #       )
+  #     )
+  #   }
+  # })
+
+
+
+}
+
+
