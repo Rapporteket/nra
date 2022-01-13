5d77f9c1fc0bb9c11c8758a7a53aa71fb2772438
kevinthon <kevin.thon@gmail.com>
2022-01-13 15:16:59 +0100

    prodklar utgave av forløpsbasert adm.tab.

--- inst/shinyApps/nra/R/modul_admtab.R
+++ inst/shinyApps/nra/R/modul_admtab.R
@@ -9,21 +9,21 @@ admtab_UI <- function(id){
   shiny::sidebarLayout(
     sidebarPanel(
       id = ns("id_adm_panel"),
-      selectInput(inputId = ns("adm_tidsenhet"), label = "Velg tidsenhet",
-                  choices = c('Måneder'=1, 'År'=2)),
+      shiny::selectInput(inputId = ns("adm_tidsenhet"), label = "Velg tidsenhet",
+                         choices = c('Måneder'=1, 'År'=2), selected = 2),
       shiny::uiOutput(ns("tab_mnd")),
       shiny::uiOutput(ns("tab_aar")),
-      sliderInput(inputId=ns("alder"), label = "Alder", min = 0,
-                  max = 130, value = c(0, 130)),
-      selectInput(inputId = ns("erMann"), label = "Kjønn",
-                  choices = c('Begge'=99, 'Kvinne'=0, 'Mann'=1)),
-      selectInput(inputId = ns("forlopstype1"), label = "Velg forløpstype",
-                  choices = c('--'=99, 'Sfinkterplastikk'=1, 'SNM'=2, 'Oppfølging 1 år'=3, 'Oppfølging 5 år'=4)),
-      uiOutput(outputId = ns('forlopstype2')),
+      shiny::uiOutput(ns("forlopstype1_ui")),
+      shiny::uiOutput(outputId = ns('forlopstype2')),
+      shiny::uiOutput(outputId = ns('regstatus_ui')),
       tags$hr(),
       actionButton(ns("reset_input"), "Nullstill valg")
     ),
     mainPanel(tabsetPanel(id= ns("admtabeller"),
+                          tabPanel("Antall forløp", value = "id_ant_forlop",
+                                   h4('Her kan få en oversikt over antall forløp i registeret basert på dato for prosedyre.
+                                   Man kan velge type forløp og registreringsstatus for de ulike delene av forløpet.'),
+                                   DTOutput(ns("Tabell_adm_forlop")), downloadButton(ns("lastNed_adm_forlop"), "Last ned tabell")),
                           tabPanel("Antall skjema", value = "id_ant_skjema",
                                    h4('Her kan du velge om du vil se registreringer per måned eller per år og for hvor
                                              lang periode. For sfinkterplastikk og SNM gjøres datofiltreringen på prosedyredato,
@@ -42,13 +42,40 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
     shinyjs::reset("id_adm_panel")
   })
 
+  output$forlopstype1_ui <- renderUI({
+    ns <- session$ns
+    if (input$admtabeller == "id_ant_skjema") {
+      selectInput(inputId = ns("forlopstype1"), label = "Velg forløpstype",
+                  choices = c('Sfinkterplastikk'=1, 'SNM'=2,
+                              'Oppfølging 1 år'=3, 'Oppfølging 5 år'=4),
+                  multiple = TRUE)}
+    else {
+      if (input$admtabeller == "id_ant_forlop") {
+        selectInput(inputId = ns("forlopstype1"), label = "Velg forløpstype",
+                    choices = c('Sfinkterplastikk'=1, 'SNM'=2),
+                    multiple = TRUE)}
+    }
+  })
+
+  output$regstatus_ui <- renderUI({
+    ns <- session$ns
+    req(input$admtabeller == "id_ant_forlop")
+    shiny::checkboxGroupInput(
+      inputId=ns("regstatus"),
+      label="Forløp med fullført:",
+      choices = c("Basisregistrering"=1, "1-årsoppfølging"=2, "5-årsoppfølging"=3),
+      selected = 1,
+      inline = FALSE
+    )
+  })
+
   output$forlopstype2 <- renderUI({
     ns <- session$ns
-    if (as.numeric(input$forlopstype1)==2) {
+    if (2 %in% as.numeric(req(input$forlopstype1))) {
       selectInput(inputId = ns("forlopstype2_verdi"), label = "SNM-type",
-                  choices = if (as.numeric(input$forlopstype1)!=1) {
-                    c('Test usikker'=1, 'Test positiv'=2, 'Revisjon'=3, 'Eksplantasjon'=4, 'Test negativ'=5)
-                  },  multiple = TRUE)
+                  choices = c('Test usikker'=1, 'Test positiv'=2, 'Revisjon'=3,
+                              'Eksplantasjon'=4, 'Test negativ'=5, 'Ikke aktuelt'=NA),
+                  multiple = TRUE)
     }
   })
 
@@ -59,7 +86,7 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
       shinyWidgets::airDatepickerInput(inputId=ns("datovalg_adm_tid_mnd"), label = "Vis til og med måned: ", minDate = '2014-01-01',
                                        maxDate = Sys.Date(), value = Sys.Date(), view = "months", minView = 'months',
                                        dateFormat = "MM yyyy", language="da"),
-      sliderInput(inputId=ns("ant_mnd"), label = "Antall måneder", min = 1, max = 24, value = 12, step = 1)
+      sliderInput(inputId=ns("ant_mnd"), label = "Antall måneder", min = 1, max = 24, value = 6, step = 1)
     )
   })
 
@@ -76,16 +103,15 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
 
   andre_adm_tab <- function() {
 
-    aux <- nraUtvalg(RegData = RegData, minald=as.numeric(input$alder[1]), maxald=as.numeric(input$alder[2]),
-                     datoFra = '2012-01-01', datoTil = '2100-01-01',
-                     erMann = as.numeric(input$erMann),forlopstype1=as.numeric(input$forlopstype1),
+    aux <- nraUtvalg(RegData = RegData, datoFra = '2012-01-01', datoTil = '2100-01-01',
+                     forlopstype1=if(!is.null(input$forlopstype1)){as.numeric(input$forlopstype1)} else {99},
                      forlopstype2=if(!is.null(input$forlopstype2_verdi)){as.numeric(input$forlopstype2_verdi)} else {99})
     aux <- aux$RegData
 
     if (input$adm_tidsenhet == 1) {
       req(input$datovalg_adm_tid_mnd)
       tilDato <- as.Date(paste0(input$datovalg_adm_tid_mnd))
-      fraDato <- tilDato %m-% months(as.numeric(input$ant_mnd)) %>% floor_date(unit="months")
+      fraDato <- tilDato %m-% months(as.numeric(input$ant_mnd)-1) %>% floor_date(unit="months")
 
       aux$mnd <- factor(format(aux$HovedDato, format='%b-%y'), levels = format(seq(fraDato, tilDato, by="month"), "%b-%y"))
 
@@ -94,7 +120,7 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
 
     if (input$adm_tidsenhet == 2) {
       req(input$datovalg_adm_tid_aar)
-      fraDato <- as.Date(input$datovalg_adm_tid_aar) %m-% years(input$ant_aar) %>% floor_date(unit="years")
+      fraDato <- as.Date(input$datovalg_adm_tid_aar) %m-% years(input$ant_aar-1) %>% floor_date(unit="years")
 
       aux$mnd <- factor(format(aux$HovedDato, format='%Y'), levels = format(seq(as.Date(fraDato),as.Date(input$datovalg_adm_tid_aar), by="year"), "%Y"))
 
@@ -147,8 +173,13 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
 
 
   admtab_forlop <- function() {
-
-    skjemaoversikt <- merge(skjemaoversikt, RegData[, c("ForlopsID", "KobletForlopsID")], by = "ForlopsID", all.x = T)
+    map_shnavn_kortnavn <- data.frame(shusnavn=unique(RegData$Sykehusnavn),
+                                      kortnavn=RegData$SenterKortNavn[match(unique(RegData$Sykehusnavn),
+                                                                            RegData$Sykehusnavn)])
+    skjemaoversikt <- merge(skjemaoversikt, RegData[, c("ForlopsID", "KobletForlopsID")],
+                            by = "ForlopsID", all.x = T)
+    skjemaoversikt$Sykehusnavn <- map_shnavn_kortnavn$kortnavn[match(skjemaoversikt$Sykehusnavn,
+                                                                     map_shnavn_kortnavn$shusnavn)]
 
     skjemaoversikt_forlop <-
       merge(skjemaoversikt[skjemaoversikt$Skjemanavn == "1A Anamnese",
@@ -173,17 +204,12 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
             by.x = "ForlopsID", by.y = "KobletForlopsID", suffixes = c("", "_oppf_5aar"), all = T)
     names(skjemaoversikt_forlop)[names(skjemaoversikt_forlop) == "SkjemaStatus"] <- "SkjemaStatus_2B"
 
-    skjemaoversikt_forlop <- merge(skjemaoversikt_forlop, RegData[, c("ForlopsID", "ForlopsType1", "ForlopsType2", "PasientID")],
+    skjemaoversikt_forlop <- merge(skjemaoversikt_forlop,
+                                   RegData[, c("ForlopsID", "ForlopsType1Num", "ForlopsType2Num", "PasientID")],
                                    by = "ForlopsID", all.x = T)
 
 
     skjemaoversikt_forlop$statusbasis <- 0
-    # skjemaoversikt_forlop$statusbasis[skjemaoversikt_forlop$SkjemaStatus_1A == 1 &
-    #                                     skjemaoversikt_forlop$SkjemaStatus_1B == 1 &
-    #                                     (skjemaoversikt_forlop$SkjemaStatus == 1 |
-    #                                        (skjemaoversikt_forlop$SkjemaStatus_2A1 == 1 &
-    #                                        skjemaoversikt_forlop$SkjemaStatus_2A2 == 1))] <- 1
-
     skjemaoversikt_forlop$statusbasis[skjemaoversikt_forlop$SkjemaStatus_1A == 1 &
                                         skjemaoversikt_forlop$SkjemaStatus_1B == 1 &
                                         (skjemaoversikt_forlop$SkjemaStatus_2B == 1 |
@@ -191,16 +217,53 @@ admtab <- function(input, output, session, reshID, RegData, userRole, hvd_sessio
                                            skjemaoversikt_forlop$ForlopsType2 %in%
                                            c("Eksplantasjon", "Revisjon"))] <- 1
 
+    if (input$adm_tidsenhet == 1) {
+      req(input$datovalg_adm_tid_mnd)
+      tilDato <- as.Date(paste0(input$datovalg_adm_tid_mnd))
+      fraDato <- tilDato %m-% months(as.numeric(input$ant_mnd)-1) %>% floor_date(unit="months")
 
-    # tmp <- skjemaoversikt_forlop[skjemaoversikt_forlop$statusbasis==0, ]
-    # tmp2 <- skjemaoversikt_forlop[skjemaoversikt_forlop$statusbasis==1, ]
+      skjemaoversikt_forlop$tid <- factor(format(skjemaoversikt_forlop$HovedDato, format='%b-%y'),
+                                          levels = format(seq(fraDato, tilDato, by="month"), "%b-%y"))
+    }
+    if (input$adm_tidsenhet == 2) {
+      req(input$datovalg_adm_tid_aar)
+      fraDato <- as.Date(input$datovalg_adm_tid_aar) %m-% years(input$ant_aar-1) %>%
+        floor_date(unit="years")
+
+      skjemaoversikt_forlop$tid <- factor(format(skjemaoversikt_forlop$HovedDato, format='%Y'),
+                                          levels = format(seq(as.Date(fraDato),as.Date(input$datovalg_adm_tid_aar),
+                                                              by="year"), "%Y"))
+    }
+
+    skjemaoversikt_forlop$SkjemaStatus_oppf_1aar[is.na(skjemaoversikt_forlop$SkjemaStatus_oppf_1aar)] <- 0
+    skjemaoversikt_forlop$SkjemaStatus_oppf_5aar[is.na(skjemaoversikt_forlop$SkjemaStatus_oppf_5aar)] <- 0
+
+    if(!is.null(input$forlopstype1)){
+      skjemaoversikt_forlop <- skjemaoversikt_forlop[skjemaoversikt_forlop$ForlopsType1Num %in%
+                                                       as.numeric(input$forlopstype1), ]
+    }
+    if(!is.null(input$forlopstype2_verdi)){
+      skjemaoversikt_forlop <- skjemaoversikt_forlop[skjemaoversikt_forlop$ForlopsType2Num %in%
+                                                       as.numeric(input$forlopstype2_verdi), ]
+    }
+
+    adm_tab <- skjemaoversikt_forlop %>% dplyr::group_by(Sykehusnavn, tid) %>%
+      dplyr::summarise(antall = sum(statusbasis == as.numeric("1" %in% input$regstatus) &
+                               SkjemaStatus_oppf_1aar == as.numeric("2" %in% input$regstatus) &
+                               SkjemaStatus_oppf_5aar == as.numeric("3" %in% input$regstatus))
+      )
+
+    adm_tab <- adm_tab[!is.na(adm_tab$tid), ] %>%
+      tidyr::spread(value = "antall", key = "tid", fill = 0, drop = FALSE)
+
+    return(adm_tab)
   }
 
-  # tmp <- skjemaoversikt[is.na(skjemaoversikt$KobletForlopsID), ]
-  # # tmp$ForlopsID[!is.na(tmp$KobletForlopsID)] <- tmp$KobletForlopsID[!is.na(tmp$KobletForlopsID)]
-  # tmp <- tmp %>% dplyr::group_by(ForlopsID) %>%
-  #   dplyr::summarise(hvd_dato_min = min(HovedDato, na.rm = T),
-  #                    hvd_dato_max = max(HovedDato, na.rm = T))
-  # tmp <- tmp[tmp$hvd_dato_min!=tmp$hvd_dato_max, ]
+  output$Tabell_adm_forlop = renderDT(
+    datatable(admtab_forlop(),
+              rownames = F,
+              options = list(pageLength = 40)
+    )
+  )
 
 }
